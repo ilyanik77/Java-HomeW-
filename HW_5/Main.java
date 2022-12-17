3e771f9308ed9b50627cc272f4cd6cdb605bfbaf
ilyanik77 <ilyakhlamov77@gmail.com>
2022-12-17 11:38:42 +0300

    HW_5

---
+++ HW_5/Main.java
@@ -0,0 +1,17 @@
+package HW_5;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Task1 task_1 = new Task1();
+        // task_1.searchFone();
+
+        // Task2 task_2 = new Task2();
+        // task_2.solution();
+
+        //Task3 task_3 = new Task3();
+        //task_3.solution();
+
+    }
+
+}
